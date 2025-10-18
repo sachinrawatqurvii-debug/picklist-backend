f60cd49f144aa18761653de9ca5fa25f6e35e42d
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-10-18 10:36:29 +0530

    picklist get by picklist id added

--- routes/picklistHistory.routes.js
+++ routes/picklistHistory.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
     createPicklistHistory,
+    getPicklistHistoryByPicklist,
     getPicklistHistoryByPicklistId,
 } from "../controllers/picklistHistory.controller.js";
 
 const router = express.Router();
 router.post("/", createPicklistHistory);
 router.get("/", getPicklistHistoryByPicklistId);
+router.get("/id", getPicklistHistoryByPicklist);
 
 export default router;
