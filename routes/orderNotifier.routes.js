27da69a021d70203ada4a3f2c818637f8dbccd1e
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-10-27 16:45:03 +0530

    notification route error fiexed

--- routes/orderNotifier.routes.js
+++ routes/orderNotifier.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import sendNotificationToEmail from "../controllers/orderNotifier.controller.js"
-const router = express();
+const router = express.Router();
 
 router.route("/send").post(sendNotificationToEmail);
 
