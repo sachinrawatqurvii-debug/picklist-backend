d77e4535ca007ad58663dda9543cdc4f1bb24ee7
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-10-27 17:36:11 +0530

    health check route added

--- routes/orderNotifier.routes.js
+++ routes/orderNotifier.routes.js
@@ -3,5 +3,7 @@ import sendNotificationToEmail from "../controllers/orderNotifier.controller.js"
 const router = express.Router();
 
 router.route("/send").post(sendNotificationToEmail);
+router.route("/health").get({health:true});
+
 
 export default router
