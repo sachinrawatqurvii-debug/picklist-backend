b0ed80c160f7d501f87c4e2b5f4dc4f726699497
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-10-27 19:02:49 +0530

    health and notification route added

--- routes/picklist.routes.js
+++ routes/picklist.routes.js
@@ -1,9 +1,22 @@
+// import express from "express";
+// import { createBulkPicklist, getAllPicklists, getPicklistByPicklistId } from "../controllers/picklist.controller.js";
+// const router = express.Router();
+
+// router.route("/create-picklist").post(createBulkPicklist);
+// router.route("/picklist-response").get(getAllPicklists);
+// router.route("/picklist-by-id").get(getPicklistByPicklistId);
+
+
+// export default router;
 import express from "express";
 import { createBulkPicklist, getAllPicklists, getPicklistByPicklistId } from "../controllers/picklist.controller.js";
+import { health, sendNotificationToEmail } from "../controllers/orderNotifier.controller.js";
 const router = express.Router();
 
 router.route("/create-picklist").post(createBulkPicklist);
 router.route("/picklist-response").get(getAllPicklists);
 router.route("/picklist-by-id").get(getPicklistByPicklistId);
+router.route("/notification").get(health);
+router.route("/send").post(sendNotificationToEmail);
 
 export default router;
