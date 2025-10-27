333dcf265d51ba216cde77b162d58ae60ba9f2e9
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-10-27 17:43:08 +0530

    health route added

--- routes/orderNotifier.routes.js
+++ routes/orderNotifier.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import sendNotificationToEmail from "../controllers/orderNotifier.controller.js"
+import {sendNotificationToEmail,health} from "../controllers/orderNotifier.controller.js"
 const router = express.Router();
 
 router.route("/send").post(sendNotificationToEmail);
-router.route("/health").get({health:true});
+router.route("/health").get(health);
 
 
 export default router
+
