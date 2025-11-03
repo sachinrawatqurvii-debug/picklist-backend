2b1c90ae8330228dcb360741f8e285a80a2974dc
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-03 14:42:51 +0530

    app.js code pasted in index.js

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+// import express from "express";
+// import "dotenv/config";
+// import { connectDB } from "./db/db.js";
+// import { globalErrorHandler } from "./middlewares/global.middleware.js";
+// import cors from "cors";
+// const app = express();
+// const PORT = process.env.PORT;
+
+// app.use(express.json({}));
+// app.use(cors());
+
+// // Routes 
+// import picklistRoutes from './routes/picklist.routes.js';
+// import picklistResponseRoutes from './routes/picklistResponse.routes.js';
+// import picklistHistoryRoutes from './routes/picklistHistory.routes.js';
+
+
+// // Registering Routes 
+
+// app.use("/api/v1/picklist", picklistRoutes);
+// app.use("/api/v1/picklist-responses", picklistResponseRoutes);
+// app.use("/api/v1/picklist-history", picklistHistoryRoutes);
+
+// app.use(globalErrorHandler)
+
+// connectDB().then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on ${PORT}`)
+//     })
+// }).catch((error) => {
+//     console.log(`Failed to connect with database error :: ${error}`);
+// })
+
 import express from "express";
 import "dotenv/config";
 import { connectDB } from "./db/db.js";
@@ -13,7 +46,13 @@ app.use(cors());
 import picklistRoutes from './routes/picklist.routes.js';
 import picklistResponseRoutes from './routes/picklistResponse.routes.js';
 import picklistHistoryRoutes from './routes/picklistHistory.routes.js';
+import orderNotificationRoutes from "./routes/orderNotifier.routes.js"
 
+
+app.use((req, res, next) => {
+    console.log("Request received:", req.method, req.originalUrl);
+    next();
+});
 
 // Registering Routes 
 
@@ -21,8 +60,13 @@ app.use("/api/v1/picklist", picklistRoutes);
 app.use("/api/v1/picklist-responses", picklistResponseRoutes);
 app.use("/api/v1/picklist-history", picklistHistoryRoutes);
 
-app.use(globalErrorHandler)
+// order notification routes 
 
+app.use("/api/v1/notification", orderNotificationRoutes)
+
+
+
+app.use(globalErrorHandler)
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on ${PORT}`)
