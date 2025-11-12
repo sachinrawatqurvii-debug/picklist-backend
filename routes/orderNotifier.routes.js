89e7b294139512e4d23d42937c07e9215b904e5e
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-12 17:51:30 +0530

    Update orderNotifier.routes.js

--- routes/orderNotifier.routes.js
+++ routes/orderNotifier.routes.js
@@ -1,16 +1,12 @@
 import express from "express";
 import { sendNotificationToEmail, health } from "../controllers/orderNotifier.controller.js";
-import { exportCSV, generatePDF } from "../controllers/exportController.js";
+
 const router = express.Router();
 
 router.route("/send").post(sendNotificationToEmail);
 router.route("/health").get(health);
 
-// router.get("/export/pdf", generatePDF);
-
-// Export CSV for cutting / up / down
-// router.get("/export/csv/:type", exportCSV);
-
 
 
 export default router
+
