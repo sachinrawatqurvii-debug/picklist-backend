8e1d5af94521e0b69bf4cdf21ad4f83dc769393e
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-17 13:47:08 +0530

    comment code deleted

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// import express from "express";
-// import "dotenv/config";
-// import { connectDB } from "./db/db.js";
-// import { globalErrorHandler } from "./middlewares/global.middleware.js";
-// import cors from "cors";
-// const app = express();
-// const PORT = process.env.PORT;
-
-// app.use(express.json({}));
-// app.use(cors());
-
-// // Routes 
-// import picklistRoutes from './routes/picklist.routes.js';
-// import picklistResponseRoutes from './routes/picklistResponse.routes.js';
-// import picklistHistoryRoutes from './routes/picklistHistory.routes.js';
-
-
-// // Registering Routes 
-
-// app.use("/api/v1/picklist", picklistRoutes);
-// app.use("/api/v1/picklist-responses", picklistResponseRoutes);
-// app.use("/api/v1/picklist-history", picklistHistoryRoutes);
-
-// app.use(globalErrorHandler)
-
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on ${PORT}`)
-//     })
-// }).catch((error) => {
-//     console.log(`Failed to connect with database error :: ${error}`);
-// })
 
 import express from "express";
 import "dotenv/config";
@@ -77,4 +45,5 @@ connectDB().then(() => {
 }).catch((error) => {
     console.log(`Failed to connect with database error :: ${error}`);
 })
+
 
