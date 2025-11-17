551af31e71fd07c1d09d6d5779edac6cfab646da
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-17 13:45:46 +0530

    getResponsesByPicklistArray controller added

--- routes/picklistResponse.routes.js
+++ routes/picklistResponse.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createBulkPicklistResponse, getResponsesByPicklistId, updatePicklistResponse } from "../controllers/picklistResponse.controller.js";
+import { createBulkPicklistResponse, getResponsesByPicklistArray, getResponsesByPicklistId, updatePicklistResponse } from "../controllers/picklistResponse.controller.js";
+
 const router = express.Router();
 
 router.route("/create-response").post(createBulkPicklistResponse)
 router.route("/picklist-id").get(getResponsesByPicklistId);
 
-
 router.route("/update").post(updatePicklistResponse)
+router.route("/report").post(getResponsesByPicklistArray)
+
 
 export default router;
