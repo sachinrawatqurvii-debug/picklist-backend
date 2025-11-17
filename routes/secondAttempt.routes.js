7e6ec596094077ccb473527f098d222655ac6444
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-17 13:46:22 +0530

    filterSecondAttemptByDate controller added

--- routes/secondAttempt.routes.js
+++ routes/secondAttempt.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getSecondAttemptByPicklist, upsertSecondAttempt } from "../controllers/secondAttemp.controller.js";
+import { filterSecondAttemptByDate, getSecondAttemptByPicklist, upsertSecondAttempt } from "../controllers/secondAttemp.controller.js";
 
 const router = express.Router();
 
 router.post("/upsert", upsertSecondAttempt);
+router.get("/date_filter", filterSecondAttemptByDate);
 router.get("/:picklist_id", getSecondAttemptByPicklist);
+
 
 export default router;
