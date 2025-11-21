ffb47be941da1f3b36dabcf4e7f1425b575e20da
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-21 12:17:16 +0530

    cutting_status update route added

--- routes/picklistAlteration.routes.js
+++ routes/picklistAlteration.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPicklistAlterationHistory, getPicklistAlterationHistoryByPicklist, getPicklistAlterationHistoryByPicklistId, updatePicklistAlterationStatus } from "../controllers/picklistAlter.controller.js";
+import { createPicklistAlterationHistory, getPicklistAlterationHistoryByPicklist, getPicklistAlterationHistoryByPicklistId, updatePicklistAlterationCuttingStatus, updatePicklistAlterationStatus } from "../controllers/picklistAlter.controller.js";
 
 const router = express.Router();
 router.post("/", createPicklistAlterationHistory);
 router.get("/", getPicklistAlterationHistoryByPicklistId);
 router.get("/id", getPicklistAlterationHistoryByPicklist);
 router.put("/update", updatePicklistAlterationStatus);
+router.put("/update_cutting", updatePicklistAlterationCuttingStatus);
 export default router;
