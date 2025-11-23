b84a231a796528f61054e666b1eb60368a1d7127
sachinrawatqurvii-debug <sachinrawatqurvii@gmail.com>
2025-11-23 11:48:58 +0530

    cliam and order mapping registered

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import "dotenv/config";
 import { connectDB } from "./db/db.js";
@@ -14,10 +13,12 @@ app.use(cors());
 import picklistRoutes from './routes/picklist.routes.js';
 import picklistResponseRoutes from './routes/picklistResponse.routes.js';
 import picklistHistoryRoutes from './routes/picklistHistory.routes.js';
-import orderNotificationRoutes from "./routes/orderNotifier.routes.js"
+import orderNotificationRoutes from "./routes/orderNotifier.routes.js";
 import secondAttemptRoutes from "./routes/secondAttempt.routes.js";
 import picklistAlterationRoutes from "./routes/picklistAlteration.routes.js";
-
+import claimRoutes from "./routes/calim.routes.js";
+import shopifyCancelledOrderRoutes from "./routes/shopifyCancelledOrder.route.js"
+import trackingAndOrderIdRoutes from "./routes/trackingAndOrderIdmapping.routes.js"
 
 app.use((req, res, next) => {
     console.log("Request received:", req.method, req.originalUrl);
@@ -31,14 +32,25 @@ app.use("/api/v1/picklist-responses", picklistResponseRoutes);
 app.use("/api/v1/picklist-history", picklistHistoryRoutes);
 
 // order notification routes 
-
 app.use("/api/v1/notification", orderNotificationRoutes)
 
 // second attempt routes 
 app.use("/api/v1/second_attempt", secondAttemptRoutes);
 
+
 // picklist alteration history routes registering 
 app.use("/api/v1/alteration", picklistAlterationRoutes)
+
+
+// claim routes registering 
+app.use("/api/v1/claim", claimRoutes);
+
+// shopify cancelled order route registering
+
+app.use("/api/v1/shopify", shopifyCancelledOrderRoutes);
+
+// tracking id and order id registering 
+app.use("/api/v1/order_id", trackingAndOrderIdRoutes)
 
 
 
@@ -50,6 +62,3 @@ connectDB().then(() => {
 }).catch((error) => {
     console.log(`Failed to connect with database error :: ${error}`);
 })
-
-
-
